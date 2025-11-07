90bbbc4774d55dd5dd723292b3d7ccf71636e13a
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:22:17 +0700

    menambahkan box  dengan warna biru muda

--- app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
@@ -1,7 +1,22 @@
 package com.example.questnavigasitugas_066.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun home(
+    modifier: Modifier = Modifier,
+    OnToFormBtnClick : () -> Unit){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF8FABD4))
+    )
+}
+
