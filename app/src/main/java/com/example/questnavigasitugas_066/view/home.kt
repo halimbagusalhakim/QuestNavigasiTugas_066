1d1f7af8ee4beb7033971ce609698b1d1946e33a
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:29:01 +0700

    menambahkan button

--- app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -58,6 +62,14 @@ fun home(
                     textAlign = TextAlign.Center,
                     fontSize = 15.sp
                 )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(1f),
+                onClick = OnToFormBtnClick,
+
+                ){
+                Text(stringResource(id = R.string.submit))
             }
 
         }
