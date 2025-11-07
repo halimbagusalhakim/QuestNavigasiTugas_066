6e477658b10d314abfcd3572d7d362a598efd188
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:26:37 +0700

    menambahkan gambar gajah

--- app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
@@ -1,16 +1,19 @@
 package com.example.questnavigasitugas_066.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -36,6 +39,13 @@ fun home(
                 fontWeight = FontWeight.Bold,
                 fontSize = 50.sp,
                 color = Color(0xFFB56BFF)
+            )
+
+            val gambar = painterResource(R.drawable.gajah2)
+            Image(
+                painter = gambar,
+                contentDescription = null,
+                modifier = Modifier.size(500.dp)
             )
 
         }
