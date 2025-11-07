9046d2c8d210531c5c8a8aca5b4b8e53099cbacc
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:39:05 +0700

    membuat fungsi tampildata

--- app/src/main/java/com/example/questnavigasitugas_066/view/tampildata.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/view/tampildata.kt
@@ -1,2 +1,14 @@
 package com.example.questnavigasitugas_066.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun TampilData(
+    modifier:Modifier = Modifier,
+    onToFormBtnClick:()-> Unit,
+    onToHomeBtnClick:()-> Unit,
+){}
