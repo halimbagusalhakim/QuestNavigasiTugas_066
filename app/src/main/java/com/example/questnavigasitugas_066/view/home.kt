6ee881e6bdf4f4a4afe7ee9a6609a97ca8a61205
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:23:13 +0700

    menambahkan coolumn yg menimpa box

--- app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
@@ -2,11 +2,15 @@ package com.example.questnavigasitugas_066.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,6 +21,14 @@ fun home(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF8FABD4))
-    )
+    ){
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 200.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+
+        }
+    }
 }
 
