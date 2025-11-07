5b415da6c7f70c1ce1484680d14b6be5743bc7d8
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:48:44 +0700

    memamnggil fungsi di navigasi ke main activity

--- app/src/main/java/com/example/questnavigasitugas_066/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestNavigasiTugas_066Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
