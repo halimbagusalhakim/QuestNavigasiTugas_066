4d50e10604c9ed9fce82353aa3fb048f1d74f44c
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:31:33 +0700

    membuat column

--- app/src/main/java/com/example/questnavigasitugas_066/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/view/Formulir.kt
@@ -1,14 +1,21 @@
 package com.example.questnavigasitugas_066.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.example.questnavigasitugas_066.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,3 +32,17 @@ fun FormulirPendaftaran(
     val paddingMedium = dimensionResource(id = R.dimen.padding_mediumform)
     val paddingLarge = dimensionResource(id = R.dimen.padding_largeform)
     val buttonHeight = dimensionResource(id = R.dimen.button_heightform)
+
+    Surface(
+        color = Color(0xFFF6ECFF),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+        }
+
