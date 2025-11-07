0ff0f60bcdec6aaf7d6320637bafd26e6caa14bb
halimbagusalhakim <halsat132@gmail.com>
2025-11-07 15:25:00 +0700

    menambahkan text selamat datang

--- app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
+++ app/src/main/java/com/example/questnavigasitugas_066/view/home.kt
@@ -6,11 +6,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_066.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,6 +32,11 @@ fun home(
             .padding(top = 200.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             ){
+            Text(text = stringResource(R.string.selamat_datang),
+                fontWeight = FontWeight.Bold,
+                fontSize = 50.sp,
+                color = Color(0xFFB56BFF)
+            )
 
         }
     }
